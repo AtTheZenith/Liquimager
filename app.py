554586ee331f36a0e1dcf114b66f66770994d5de
zen <157610281+AtTheZenith@users.noreply.github.com>
2024-09-29 19:33:12 +0300

    v0.4.0
    [+] make x and y position text forms functional
    [+] add numexpr library for x and y position text forms to evaluate expressions safely
    [+] add input filter for x and y position text forms
    [+] add check for refresh_image() to check size to prevent errors
    [*] fix bugs and remove unused items
    [*] change style and position of x and y text forms
    [-] remove gray placeholder text
    [-] remove initial textbox range '0-400'
    [-] remove NumberField class
    
    tl;dr:
    
    [+] finish x and y position text form (add functionality, add safe expression eval, add input filter, change style)
    [+] add numexpr library
    [*] fix bugs and remove unused items
    [-] remove NumberField class

--- app.py
+++ app.py
@@ -1,224 +1,271 @@
 """Liquimager App."""
 
 from time import time
-from typing import Callable#, Union
+from typing import Callable
 
 import customtkinter as ctk
+from numexpr import NumExpr as evaluate
 from PIL import Image, ImageDraw, ImageFont
 
 from ui_elements import (
-    FileButton,
-    FileButtonSeperator,
-    FramesSeperator,
-    ImageFrame,
-    ImageLabel,
-    Label,
-    MainFrame,
-    NumberField,
-    Slider,
-    # TextField
+	FileButton,
+	FileButtonSeperator,
+	FramesSeperator,
+	ImageFrame,
+	ImageLabel,
+	Label,
+	MainFrame,
+	Slider,
+	TextField
 )
 
 
 class ImageManager:
-    """Image Manager Class because my procedural methods didn't work."""
+	"""Image Manager Class because my procedural methods didn't work."""
 
-    def __init__(self):
-        self._orig_image = None
-        self._image = None
-        self._width_image = 0
-        self._height_image = 0
+	def __init__(self):
+		self._orig_image = None
+		self._image = None
+		self._width_image = 0
+		self._height_image = 0
 
-    def set_new_image(self, new_image: Image.Image) -> None:
-        """Sets a new image.
-        new_image: The image to be set.
-        \n*args: None | **kwargs: None
-        """
+	def set_new_image(self, new_image: Image.Image) -> None:
+		"""Sets a new image.
+		new_image: The image to be set.
+		\n*args: None | **kwargs: None
+		"""
 
-        self._orig_image = new_image.convert("RGBA")
-        self._image = self._orig_image.copy()
-        self._width_image, self._height_image = self._orig_image.size
+		self._orig_image = new_image.convert("RGBA")
+		self._image = self._orig_image.copy()
+		self._width_image, self._height_image = self._orig_image.size
 
-    def update_image(self, new_image: Image.Image) -> None:
-        """Updates the current image.
-        \nnew_image: The image to be set.
-        \n*args: None | **kwargs: None
-        """
+	def update_image(self, new_image: Image.Image) -> None:
+		"""Updates the current image.
+		\nnew_image: The image to be set.
+		\n*args: None | **kwargs: None
+		"""
 
-        self._image = new_image.convert("RGBA")
-        self._width_image, self._height_image = new_image.size
+		self._image = new_image.convert("RGBA")
+		self._width_image, self._height_image = new_image.size
 
-    def delete_image(self) -> None:
-        """Deletes the selected image.
-        \nnew_image: The image to be set.
-        \n*args: None | **kwargs: None
-        """
+	def delete_image(self) -> None:
+		"""Deletes the selected image.
+		\nnew_image: The image to be set.
+		\n*args: None | **kwargs: None
+		"""
 
-        self._orig_image, self._image, self._width_image, self._height_image = (None, None,0,0)
+		self._orig_image, self._image, self._width_image, self._height_image = (None, None,0,0)
 
-    def fit_image(self, image: Image.Image, width: int, height: int) -> tuple:
-        """Fits the image to a given width and height.
-        \nimage: The image to fit.
-        \nwidth: The width to fit the image to.
-        \nheight: The height to fit the image to.
-        \n*args: None | **kwargs: None
-        """
+	def fit_image(self, image: Image.Image, width: int, height: int) -> tuple:
+		"""Fits the image to a given width and height.
+		\nimage: The image to fit.
+		\nwidth: The width to fit the image to.
+		\nheight: The height to fit the image to.
+		\n*args: None | **kwargs: None
+		"""
 
-        x_scale = image.width / width
-        y_scale = image.height / height
+		x_scale = image.width / width
+		y_scale = image.height / height
 
-        scale = max(x_scale, y_scale)
+		scale = max(x_scale, y_scale)
 
-        return (int(self._width_image / scale), int(self._height_image / scale))
+		return (int(self._width_image / scale), int(self._height_image / scale))
 
-    def get_orig_image(self) -> Image.Image:
-        """Returns the original image.
-        \n*args: None | **kwargs: None
-        """
-        return (self._orig_image and self._orig_image or Image.new(mode="RGBA", size=(0, 0)))
+	def get_orig_image(self) -> Image.Image:
+		"""Returns the original image.
+		\n*args: None | **kwargs: None
+		"""
+		return (self._orig_image and self._orig_image or Image.new(mode="RGBA", size=(0, 0)))
 
-    def get_image(self) -> Image.Image:
-        """Returns the current image.
-        \n*args: None | **kwargs: None
-        """
-        return self._image or Image.new(mode="RGBA", size=(0, 0))
+	def get_image(self) -> Image.Image:
+		"""Returns the current image.
+		\n*args: None | **kwargs: None
+		"""
+		return self._image or Image.new(mode="RGBA", size=(0, 0))
 
-    def get_image_size(self) -> tuple:
-        """Returns the size of the current image as a tuple.
-        \n*args: None | **kwargs: None
-        """
-        return (self._width_image != 0 and self._width_image or 400, self._width_image != 0 and self._width_image or 400)
+	def get_image_size(self) -> tuple:
+		"""Returns the size of the current image as a tuple.
+		\n*args: None | **kwargs: None
+		"""
+		return (self._width_image != 0 and self._width_image or 400, self._height_image != 0 and self._height_image or 400)
 
 
 im = ImageManager()
 
 
 class App(ctk.CTk):
-    """App class, call 'App().mainloop()' to run."""
+	"""App class, call 'App().mainloop()' to run."""
 
-    def __init__(self):
-        super().__init__()
-        self.title("Liquimager")
-        self.geometry("1200x600")
-        self.resizable(False, False)
+	def __init__(self):
+		super().__init__()
+		self.title("Liquimager")
+		self.geometry("1200x600")
+		self.resizable(False, False)
 
-        ##################################################
-        # UI Elements                                    #
-        ##################################################
+		##################################################
+		# UI Elements                                    #
+		##################################################
 
-        main_frame = MainFrame(self)
+		main_frame = MainFrame(self)
 
-        file_button = FileButton(main_frame)
-        FileButtonSeperator(main_frame)
+		file_button = FileButton(main_frame)
+		FileButtonSeperator(main_frame)
 
-        Label(master=main_frame, position=(17, 100), text="Horizontal Position")
-        Label(master=main_frame, position=(17, 150), text="Vertical Position")
+		Label(master=main_frame, position=(17, 98), text="Horizontal Position")
+		Label(master=main_frame, position=(17, 148), text="Vertical Position")
 
-        x_slider = Slider(master=main_frame, position=(24, 125))
-        y_slider = Slider(master=main_frame, position=(24, 175))
-        
-        x_entry = NumberField(master=main_frame, size=(120,24), position=(210,103))
-        y_entry = NumberField(master=main_frame, size=(120,24), position=(210,153))
+		x_slider = Slider(master=main_frame, position=(24, 125))
+		y_slider = Slider(master=main_frame, position=(24, 175))
 
-        FramesSeperator(self)
+		x_entry = TextField(master=main_frame, size=(120,20), position=(210,101))
+		y_entry = TextField(master=main_frame, size=(120,20), position=(210,151))
 
-        image_frame = ImageFrame(self)
-        image_label = ImageLabel(image_frame)
+		x_val = ctk.StringVar(master=x_entry, value='0', name='x')
+		y_val = ctk.StringVar(master=y_entry, value='0', name='y')
 
-        ##################################################
-        # Callbacks                                      #
-        ##################################################
+		FramesSeperator(self)
 
-        def set_option_access(access: bool) -> None:
-            """Disables/Enables the UI Elements.
-                \naccess: bool (Enable or Disable)
-                \n*args: None | **kwargs: None
-            """
+		image_frame = ImageFrame(self)
+		image_label = ImageLabel(image_frame)
 
-            for item in [x_slider, x_entry, y_slider, y_entry]:
-                item.configure(state=(access and "normal" or "disabled"))
-                if isinstance(item, Slider):
-                    item.set(0)
-                    item.configure(to=im.get_image_size()[1] or 400)
-                elif isinstance(item, NumberField):
-                    item.configure(placeholder_text=f'0-{im.get_image_size()[1] == 0 and 400 or im.get_image_size()[1]}')
+		##################################################
+		# Callbacks                                      #
+		##################################################
 
-        set_option_access(access=False)
+		def set_option_access(access: bool) -> None:
+			"""Disables/Enables the UI Elements.
+				\naccess: bool (Enable or Disable)
+				\n*args: None | **kwargs: None
+			"""
+
+			for item in [x_slider, x_entry, y_slider, y_entry, x_val, y_val]:
+				if not isinstance(item, ctk.StringVar):
+					item.configure(state=(access and "normal" or "disabled"))
+				if isinstance(item, Slider):
+					item.set(0)
+					axis = 0 if item == x_slider else 1
+					item.configure(
+						to=im.get_image_size()[axis],
+						number_of_steps=im.get_image_size()[axis]
+					)
+				elif isinstance(item, ctk.StringVar):
+					item.set('0')
+
+		set_option_access(access=False)
 
 
-        def refresh_closure() -> Callable:
-            last_call = time()
-            image = im.get_orig_image()
+		def refresh_closure() -> Callable:
+			last_call = time()
+			image = im.get_orig_image()
 
-            def refresh_image(_) -> None:
-                nonlocal last_call, image
-                if time() - last_call < (1/120):
-                    return
+			def _() -> None:
+				nonlocal last_call, image
 
-                last_call = time()
+				if (im.get_image().size == (0,0)):
+					return
 
-                x_pos, y_pos = x_slider.get(), y_slider.get()
+				if time() - last_call < (1/120):
+					return
 
-                if im.get_orig_image() != image:
-                    image = im.get_orig_image()
+				last_call = time()
 
-                watermark = image.copy()
-                drawing = ImageDraw.Draw(watermark, "RGBA")
+				if im.get_orig_image() != image:
+					image = im.get_orig_image()
 
-                font_name = "arial"
-                font = ImageFont.truetype(f"C:/Windows/Fonts/{font_name}.ttf", size=40)
+				watermark = image.copy()
+				drawing = ImageDraw.Draw(watermark, "RGBA")
 
-                drawing.text(
-                    xy   = (x_pos, y_pos),
-                    text = "Watermark",
-                    fill = (255, 255, 255, 128),
-                    font = font,
-                )
+				font_name = "arial"
+				font = ImageFont.truetype(f"C:/Windows/Fonts/{font_name}.ttf", size=40)
 
-                im.update_image(watermark)
+				drawing.text(
+					xy   = (x_slider.get(), y_slider.get()),
+					text = "Watermark",
+					fill = (255, 255, 255, 128),
+					font = font,
+				)
 
-                image_label.configure(
-                    image=ctk.CTkImage(
-                        dark_image=im.get_image(),
-                        size=im.fit_image(image=im.get_image(), width=720, height=520),
-                    )
-                )
+				im.update_image(watermark)
 
-            return refresh_image
+				image_label.configure(
+					image=ctk.CTkImage(
+						dark_image=im.get_image(),
+						size=im.fit_image(image=im.get_image(), width=720, height=520),
+					)
+				)
 
-        ref_fun = refresh_closure()
+			return _
 
-        def open_image() -> None:
-            """Callback for file_button, manages the image selection.
-            *args: None | **kwargs: None
-            """
+		refresh_image: Callable = refresh_closure()
 
-            if im.get_image().size == (0, 0):
-                file_path = ctk.filedialog.askopenfilename(filetypes=[("Image Files", "*.png;*.jpg;*.jpeg;*.jfif")])
+		def open_image() -> None:
+			"""Callback for file_button, manages the image selection.
+			*args: None | **kwargs: None
+			"""
 
-                if file_path:
-                    im.set_new_image(new_image=Image.open(file_path))
-                    file_button.configure(text="Remove File")
-                    set_option_access(access=True)
-                    image_label.configure(text="")
-                    ref_fun(0)
-            else:
-                im.delete_image()
-                file_button.configure(text="Open File")
-                set_option_access(access=False)
-                image_label.configure(
-                    text="No Image Selected",
-                    image=ctk.CTkImage(dark_image=Image.new(mode="RGBA", size=(0, 0)), size=(0, 0)),
-                )
+			if im.get_image().size == (0, 0):
+				file_path = ctk.filedialog.askopenfilename(filetypes=[("Image Files", "*.png;*.jpg;*.jpeg;*.jfif")])
 
-        ##################################################
-        # Callback binding                               #
-        ##################################################
+				if file_path:
+					im.set_new_image(new_image=Image.open(file_path))
+					file_button.configure(text="Remove File")
+					set_option_access(access=True)
+					image_label.configure(text="")
+					refresh_image()
+			else:
+				im.delete_image()
+				file_button.configure(text="Open File")
+				set_option_access(access=False)
+				image_label.configure(
+					text="No Image Selected",
+					image=ctk.CTkImage(dark_image=Image.new(mode="RGBA", size=(0, 0)), size=(0, 0)),
+				)
 
-        file_button.configure(command=open_image)
-        x_slider.configure(command=ref_fun)
-        y_slider.configure(command=ref_fun)
+		def x_slider_func(num) -> None:
+			x_val.set(str(int(num)))
+
+		def y_slider_func(num) -> None:
+			y_val.set(str(int(num)))
+
+		def x_val_callback(str1, str2, str3) -> None:
+			if '..' in x_val.get() or not set(x_val.get()).issubset(set('0123456789+-*/.')):
+				x_val.set(''.join(filter('0123456789+-*/.'.__contains__, x_val.get().replace('..', '.'))))
+				return
+
+			if str(x_slider.get()) != (x_val.get()):
+				try:
+					x_slider.set(evaluate(ex=x_val.get())().astype(int).item())
+					refresh_image()
+				except Exception:
+					return
+
+		def y_val_callback(str1, str2, str3) -> None:
+			if '..' in y_val.get() or not set(y_val.get()).issubset(set('0123456789+-*/.')):
+				y_val.set(''.join(filter('0123456789+-*/'.__contains__, y_val.get().replace('..', '.'))))
+				return
+
+			if str(y_slider.get()) != (y_val.get()):
+				try:
+					y_slider.set(evaluate(ex=y_val.get())().astype(int).item())
+					refresh_image()
+				except Exception:
+					return
+
+		##################################################
+		# Callback binding                               #
+		##################################################
+
+		file_button.configure(command=open_image)
+
+		x_slider.configure(command=x_slider_func)
+		y_slider.configure(command=y_slider_func)
+
+		x_entry.configure(textvariable=x_val)
+		y_entry.configure(textvariable=y_val)
+
+		x_val.trace_add(mode='write', callback=x_val_callback)
+		y_val.trace_add(mode='write', callback=y_val_callback)
 
 
 if __name__ == "__main__":
-    App().mainloop()
+	App().mainloop()
--- ui_elements.py
+++ ui_elements.py
@@ -3,196 +3,171 @@
 import customtkinter as ctk
 
 COLORS = {
-    'MainButton': '#2D5FC4',
-    'MainButtonHover': '#2255BB',
-    'AltButton': '#DDDDDD',
-    'AltButtonHover': '#FFFFFF',
-    'MainText': '#FFFFFF',
-    'AltText': '#666666',
-    'Window': '#242424',
-    'MainFrame': '#2B2B2B',
-    'AltFrame': '#444444'
+    "MainButton": "#2D5FC4",
+    "MainButtonHover": "#2255BB",
+    "AltButton": "#DDDDDD",
+    "AltButtonHover": "#FFFFFF",
+    "MainText": "#FFFFFF",
+    "AltText": "#666666",
+    "Window": "#242424",
+    "MainFrame": "#2B2B2B",
+    "AltFrame": "#444444",
 }
+
 
 class Label(ctk.CTkLabel):
     """Text label to label sliders and dropdowns.
-        \nmaster: The parent UI element.
-        \nposition: The position of the slider (an xy tuple).
-        \n*args: None | **kwargs: None
+    \nmaster: The parent UI element.
+    \nposition: The position of the slider (an xy tuple).
+    \n*args: None | **kwargs: None
     """
+
     def __init__(self, master: ctk.CTkBaseClass, position: tuple, text: str):
         super().__init__(
             master,
-            text          = text,
-            font          = ('Montserrat', 13),
-            text_color    = COLORS['AltText'],
-            bg_color      = 'transparent',
-            corner_radius = 12
+            text=text,
+            font=("Montserrat", 13),
+            text_color=COLORS["AltText"],
+            bg_color="transparent",
+            corner_radius=12,
         )
-        self.place(x = position[0], y = position[1])
+        self.place(x=position[0], y=position[1])
 
 
 class Slider(ctk.CTkSlider):
     """Slider for adjusting the position of the text.
-        \nmaster: The pareadnt UI element.
-        \nposition: The position of the slider (an xy tuple).
-        \n*args: None | **kwargs: None
+    \nmaster: The pareadnt UI element.
+    \nposition: The position of the slider (an xy tuple).
+    \n*args: None | **kwargs: None
     """
+
     def __init__(self, master: ctk.CTkBaseClass, position: tuple):
         super().__init__(
             master,
-            from_                = 0,
-            to                   = 400,
-            number_of_steps      = 400,
-            width                = 312,
-            height               = 18,
-            corner_radius        = 12,
-            orientation          = 'horizontal',
-            bg_color             = 'transparent',
-            fg_color             = COLORS['AltFrame'],
-            button_color         = COLORS['AltButton'],
-            progress_color       = COLORS['MainButton'],
-            button_hover_color   = COLORS['AltButtonHover'],
-            button_corner_radius = 12
+            from_=0,
+            to=400,
+            number_of_steps=400,
+            width=312,
+            height=18,
+            corner_radius=12,
+            orientation="horizontal",
+            bg_color="transparent",
+            fg_color=COLORS["AltFrame"],
+            button_color=COLORS["AltButton"],
+            progress_color=COLORS["MainButton"],
+            button_hover_color=COLORS["AltButtonHover"],
+            button_corner_radius=12,
         )
-        self.place(x = position[0], y = position[1])
+        self.place(x=position[0], y=position[1])
 
 
 class TextField(ctk.CTkEntry):
     """Text field for entering text.
-        \nmaster: The parent UI element.
-        \nsize: The size of the text field
-        \nposition: The position of the text field (an xy tuple).
-        \n*args: None | **kwargs: None
+    \nmaster: The parent UI element.
+    \nsize: The size of the text field
+    \nposition: The position of the text field (an xy tuple).
+    \n*args: None | **kwargs: None
     """
+
     def __init__(self, master: ctk.CTkBaseClass, size: tuple, position: tuple):
         super().__init__(
             master,
-            width                  = size[0],
-            height                 = size[1],
-            corner_radius          = 5, 
-            border_width           = 2,
-            bg_color               = 'transparent',
-            fg_color               = COLORS['MainFrame'],
-            placeholder_text_color = COLORS['AltFrame'],
-            text_color             = COLORS['MainText'],
-            border_color           = COLORS['AltFrame'],
-            placeholder_text       = '0-400',
-            font                   = ('Montserrat', 12)
+            width=size[0],
+            height=size[1],
+            corner_radius=5,
+            border_width=2,
+            bg_color="transparent",
+            fg_color=COLORS["MainFrame"],
+            placeholder_text_color=COLORS["MainText"],
+            text_color=COLORS["MainText"],
+            border_color=COLORS["AltFrame"],
+            placeholder_text="0",
+            font=("Montserrat", 12),
         )
-        self.place(x = position[0], y = position[1])
-
-
-class NumberField(ctk.CTkEntry):
-    """Number field for entering an integer.
-        \nmaster: The parent UI element.
-        \nsize: The size of the text field
-        \nposition: The position of the text field (an xy tuple).
-        \n*args: None | **kwargs: None
-    """
-    def __init__(self, master: ctk.CTkBaseClass, size: tuple, position: tuple):
-        super().__init__(
-            master,
-            width                  = size[0],
-            height                 = size[1],
-            corner_radius          = 5, 
-            border_width           = 2,
-            bg_color               = 'transparent',
-            fg_color               = COLORS['MainFrame'],
-            placeholder_text_color = COLORS['AltFrame'],
-            text_color             = COLORS['MainText'],
-            border_color           = COLORS['AltFrame'],
-            placeholder_text       = '0-400',
-            font                   = ('Montserrat', 12),
-            validate               = 'key',
-            validatecommand        = (master.register(self.validate_input), '%S')
-        )
-        self.place(x = position[0], y = position[1])
-
-    def validate_input(self, input_text):
-        return input_text.isdigit()
+        self.place(x=position[0], y=position[1])
 
 
 class FileButton(ctk.CTkButton):
     """Button, for selecting an image file/removing selected image file.
-        \nmaster: The parent UI element.
-        \n*args: None | **kwargs: None
+    \nmaster: The parent UI element.
+    \n*args: None | **kwargs: None
     """
+
     def __init__(self, master: ctk.CTkBaseClass):
         super().__init__(
             master,
-            text          = 'Open File',
-            font          = ('Montserrat', 24),
-            corner_radius = 12,
-            fg_color      = COLORS['MainButton'],
-            hover_color   = COLORS['MainButtonHover'],
-            width         = 320,
-            height        = 50
+            text="Open File",
+            font=("Montserrat", 24),
+            corner_radius=12,
+            fg_color=COLORS["MainButton"],
+            hover_color=COLORS["MainButtonHover"],
+            width=320,
+            height=50,
         )
 
-        self.image_file = None
-        self.place(x = 20, y = 20)
+        self.place(x=20, y=20)
 
 
 class FileButtonSeperator(ctk.CTkFrame):
     """Frames Seperator, that will separate the main frame from the image frame.
-        \nmaster: The parent UI element.
-        \n*args: None | **kwargs: None
+    \nmaster: The parent UI element.
+    \n*args: None | **kwargs: None
     """
+
     def __init__(self, master: ctk.CTkBaseClass):
         super().__init__(
-            master,
-            corner_radius = 2,
-            fg_color      = COLORS['AltFrame'],
-            width         = 300,
-            height        = 2
+            master, corner_radius=2, fg_color=COLORS["AltFrame"], width=300, height=2
         )
-        self.place(x = 30, y = 89)
+        self.place(x=30, y=89)
 
 
 class MainFrame(ctk.CTkFrame):
     """Main Frame, that will contain all the main buttons.
-        \nmaster: The parent UI element.
-        \n*args: None | **kwargs: None
+    \nmaster: The parent UI element.
+    \n*args: None | **kwargs: None
     """
+
     def __init__(self, master: ctk.CTk):
-        super().__init__(master, corner_radius = 24, width = 360, height = 560)
-        self.place(x = 20, y = 20)
+        super().__init__(master, corner_radius=24, width=360, height=560)
+        self.place(x=20, y=20)
 
 
 class FramesSeperator(ctk.CTkFrame):
     """Frames Seperator, that will separate the main frame from the image frame.
-        \nmaster: The parent UI element.
-        \n*args: None | **kwargs: None
+    \nmaster: The parent UI element.
+    \n*args: None | **kwargs: None
     """
+
     def __init__(self, master: ctk.CTk):
-        super().__init__(master, corner_radius = 8, width = 4, height = 560)
-        self.place(x = 398, y = 20)
+        super().__init__(master, corner_radius=8, width=4, height=560)
+        self.place(x=398, y=20)
 
 
 class ImageLabel(ctk.CTkLabel):
     """Label, that will display the selected image.
-        \nmaster: The parent UI element.
-        \n*args: None | **kwargs: None
+    \nmaster: The parent UI element.
+    \n*args: None | **kwargs: None
     """
+
     def __init__(self, master: ctk.CTkBaseClass):
         super().__init__(
             master,
-            corner_radius = 8,
-            text          = 'No Image Selected',
-            font          = ('Montserrat', 24),
-            width         = 720,
-            height        = 520
+            corner_radius=8,
+            text="No Image Selected",
+            font=("Montserrat", 24),
+            width=720,
+            height=520,
         )
-        self.place(x = 20, y = 20)
+        self.place(x=20, y=20)
         self.image = None
 
 
 class ImageFrame(ctk.CTkFrame):
     """Image Frame, that will contain the image preview.
-        \nmaster: The parent UI element.
-        \n*args: None | **kwargs: None
+    \nmaster: The parent UI element.
+    \n*args: None | **kwargs: None
     """
+
     def __init__(self, master: ctk.CTk):
-        super().__init__(master, corner_radius = 24, width = 760, height = 560)
-        self.place(x = 420, y = 20)
+        super().__init__(master, corner_radius=24, width=760, height=560)
+        self.place(x=420, y=20)
